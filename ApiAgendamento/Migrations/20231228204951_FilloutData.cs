94c9ed5ad430d5bd8622112f2f01ac36dc687ef9
joaoMonaro <60273165+joaoMonaro@users.noreply.github.com>
2023-12-28 18:07:04 -0300

    AddUserAdmin
    Add admin user for tests

---
+++ ApiAgendamento/Migrations/20231228204951_FilloutData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiAgendamento.Migrations
+{
+    /// <inheritdoc />
+    public partial class FilloutData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO \"Usuarios\" (\"Login\", \"Senha\", \"Tipo\", \"Email\") VALUES ('admin', 'admin', true, 'admin@admin.com')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM usuarios WHERE Login = 'admin'");
+        }
+    }
+}
