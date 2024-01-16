0eb3d48fec962fba45b10bbca4168d81862c7926
joaoMonaro <60273165+joaoMonaro@users.noreply.github.com>
2024-01-15 23:23:49 -0300

    Adiciona Usuario Controller
    Adicionado a classe de controle para usuarios

---
+++ ApiAgendamento/Controllers/UsuariosController.cs
@@ -0,0 +1,31 @@
+﻿using ApiAgendamento.Context;
+using ApiAgendamento.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiAgendamento.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UsuariosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<UsuarioModel>> Get()
+        {
+            var usuarios = _context.UsuarioModel.ToList();
+
+            if (usuarios is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(usuarios);
+        }
+    }
+}
